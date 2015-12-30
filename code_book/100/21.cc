f45e48f7104d8704fec3597eae6937a905300937
JM Han <13240943@qq.com>
2015-12-30 09:25:01 -0800

    update code 20151230

--- code_book/100/21.cc
+++ code_book/100/21.cc
@@ -4,34 +4,47 @@
 using namespace std;
 
 void print(int* aux, int n) {
-	for (int i = 0; i < n; i++)
+	for (int i = 1; i <= n; i++)
 		if (aux[i])
 			cout << i << " ";
 	cout << endl;
 }
 
 void helper(int m, int cur, int* aux, int n) {
-	if (m == 0)
+	if (m == 0){
+		cout << "Got one: " << endl;
 		print(aux, n);
-	if (m <= 0 || cur == n)
+		//if equal, no need to calculate further recurse
+		//cause, further add will make it unequal
 		return;
+	}
+	// m<0 for select, but sum too big
+	//cur > n for selection out of range
+	if (m < 0 || cur >  n){
+		//cout << "calulate: " << endl;
+		//for (int i = 1; i <= n; i++)
+		//	cout << aux[i] << " ";
+		//cout << endl;
+		return;
+	}
 
-	// 不选cur
+	// unselect cur
 	helper(m, cur + 1, aux, n);
 
-	// 选cur
+	// select cur
 	aux[cur] = 1;
 	helper(m - cur, cur + 1, aux, n);
-	aux[cur] = 0;  // 回溯
+	aux[cur] = 0;  // backout 
 }
 
 void find_combi(int n, int m) {
 	if (n > m) 
 		find_combi(m, m);
-
-	int* aux = new int[n];  // aux[i] = 1，表示选择i
-	memset(aux, 0, n * sizeof(int));
-	helper(m, 0, aux, n);
+	else {
+		int* aux = new int[n];  // aux[i] = 1 represent selected
+		memset(aux, 0, n * sizeof(int));
+		helper(m, 1, aux, n);
+	}
 }
 
 int main() {
