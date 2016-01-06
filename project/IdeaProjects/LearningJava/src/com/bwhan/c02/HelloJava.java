b19e8cb31070f8dcaddc3719ae89ad03fc2fe966
JM Han <13240943@qq.com>
2016-01-06 15:59:23 -0800

    update code @homt Jan 1 2016

--- project/IdeaProjects/LearningJava/src/com/bwhan/c02/HelloJava.java
+++ project/IdeaProjects/LearningJava/src/com/bwhan/c02/HelloJava.java
@@ -1,15 +1,14 @@
 package c02;
 
-import tool.util;
+import static tool.util.prt;
 
 /**
  * Created by Jimmy on 2015/4/6.
  */
 
-
 public class HelloJava{
     public  static void main(String[] args){
-	    util.prt("abc");
+	    prt("abc");
 	    System.out.println("Hello Java!");
     }
 }
