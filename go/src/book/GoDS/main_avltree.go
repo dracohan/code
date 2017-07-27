32991dde70084fb6435108d4ab2eb1e71f69e7b5
JM Han <13240943@qq.com>
2017-07-27 20:40:07 +0800

    update

---
+++ go/src/book/GoDS/main_avltree.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"book/GoDS/tree/avltree"
+	"fmt"
+)
+
+func main() {
+	avlt := avltree.NewWithIntComparator()
+	avlt.Insert(4)
+	avlt.Insert(3)
+	avlt.Insert(6)
+	avlt.Insert(0)
+	avlt.Insert(2)
+	avlt.Insert(3)
+	avlt.PrintTree()
+	fmt.Println(avlt.Contains(3))
+	fmt.Println(avlt.FindMax())
+	fmt.Println(avlt.FindMin())
+	avlt.Remove(3)
+	avlt.PrintTree()
+}
