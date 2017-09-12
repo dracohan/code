8fe6b54117f7ebc89a2167ef705824ae601b5f65
JM Han <13240943@qq.com>
2017-09-12 22:33:25 +0800

    update

--- go/src/book/gobyexample/2values/values.go
+++ go/src/book/gobyexample/2values/values.go
@@ -5,5 +5,10 @@ import (
 )
 
 func main() {
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1=", 1+1)
+	fmt.Println("8.0/3.0 = ", 8.0/3.0)
+	fmt.Println(true && false)
 	fmt.Println(true || false)
+	fmt.Println(!true)
 }
