72b9481035a095d9942e738754d6125edb6c6fee
JM Han <13240943@qq.com>
2017-09-29 07:14:47 +0800

    update

---
+++ go/src/ex/NOWCODER/1_treeHeight/m.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type node struct {
+	parent *node
+	index  int
+	height int
+}
+
+func add(pi, in int) {
+	if in == 0 {
+		root := &node{nil, 0, 0}
+		node := &node{root, 0, 1}
+		nodes[in] = node
+		return
+	}
+
+	if pn, ok := nodes[pi]; ok {
+		node := &node{pn, in, pn.height + 1}
+		nodes[in] = node
+	} else {
+		fmt.Println("parent not found")
+	}
+}
+
+var ar []string
+var num int
+var nodes map[int]*node
+
+func main() {
+	text := bufio.NewScanner(os.Stdin)
+	first := true
+	for text.Scan() {
+		line := text.Text()
+		if first {
+			num = strconv.Atoi(line)
+			first = false
+		} else {
+			r := strings.Split(line, " ")
+			pi := strconv.Atoi(r[0])
+			in := strconv.Atoi(r[1])
+			add(pi, in)
+		}
+	}
+
+}
