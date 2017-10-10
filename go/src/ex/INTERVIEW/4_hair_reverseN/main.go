1e6f151d4de7e8e65733e433f73c5dfa6b3025ff
JM Han <13240943@qq.com>
2017-10-10 19:36:45 +0800

    update

--- go/src/ex/INTERVIEW/4_hair_reverseN/main.go
+++ go/src/ex/INTERVIEW/4_hair_reverseN/main.go
@@ -20,12 +20,54 @@ func printList(head *node) {
 	for p = head; p.next != nil; p = p.next {
 		fmt.Printf("%d->", p.data)
 	}
-	fmt.Printf("%d", p.data)
+	fmt.Printf("%d\n", p.data)
 }
+
+func reverseN(head *node, num int) *node {
+	var prev, curr, next *node
+	var count int
+
+	if num == 0 || num == 1 {
+		return head
+	}
+
+	prev = head
+	curr = prev.next
+
+	for num-1 > count {
+		if curr != nil {
+			next = curr.next
+			curr.next = prev
+			prev = curr
+			curr = next
+			count++
+		} else {
+			curr = prev.next
+			prev.next = nil
+			for count > 0 {
+				next = curr.next
+				curr.next = prev
+				prev = curr
+				curr = next
+				count--
+			}
+			return prev
+		}
+	}
+	if next != nil {
+		head.next = reverseN(next, num)
+	} else {
+		head.next = nil
+	}
+	return prev
+}
+
 func main() {
 	var head *node
-	for i := 10; i > 0; i-- {
+	for i := 11; i > 0; i-- {
 		head = add(head, i)
 	}
+	printList(head)
+	head = reverseN(head, 3)
 	printList(head)
 }
