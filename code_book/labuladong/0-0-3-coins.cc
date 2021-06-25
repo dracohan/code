2033b01e8207647b3efbaab363b4831a6c47525c
Jimmy.Han <13240943@qq.com>
2021-06-25 19:04:25 +0800

    update@off

--- code_book/labuladong/0-0-3-coins.cc
+++ code_book/labuladong/0-0-3-coins.cc
@@ -21,65 +21,32 @@ int coinChange(vector<int> &coins, int amount) {
     return -1;
 }
 
-map<int, int> memo;
-int coinChange_with_memo(vector<int> &coins, int amount) {
-  if (amount == 0) return 0;
-  if (amount < 0) return -1;
-
-  if ((memo[amount]) != 0) return memo[amount];
-
-  int res = amount + 1;
-  for (int i = 0; i < coins.size(); i++) {
-    int sub = coinChange_with_memo(coins, amount - coins[i]);
-
-    if (sub == -1) continue;
-    if (res > sub + 1) res = sub + 1;
-
-    memo[amount - coins[i]] = sub;
-  }
-
-  // if (amount == 11) // final
-  //   cout << "break" << endl;
-
-  if (res > 0 && res < amount + 1)
-    return res;
-  else
-    return -1;
-}
-
 vector<int> count;
 int dp(vector<int> &coins, int amount) {
   if (amount == 0) return 0;
   if (amount < 0) return -1;
 
-  if ((count[amount]) != 0) return count[amount];
+  if ((count[amount]) != -2) return count[amount];
 
   int res = amount + 1;
   for (int i = 0; i < coins.size(); i++) {
     int sub = dp(coins, amount - coins[i]);
-
     if (sub == -1) continue;
     if (res > sub + 1) res = sub + 1;
   }
 
-  // if (amount == 11) 
-  //   cout << "break" << endl;
   if (res == amount + 1)
     count[amount] = -1;
   else
     count[amount] = res;
 
-  if (res > 0 && res < amount + 1) {
-    return res;
-  }
-
-  return -1;
+  return count[amount];
 }
 
 // int dp(vector<int> &coins, int rem) {
 //   if (rem < 0) return -1;
 //   if (rem == 0) return 0;
-//   if (count[rem - 1] != 0) return count[rem - 1];
+//   if (count[rem - 1] != -2) return count[rem - 1];
 //   cout << "calc: " << rem << endl;
 //   int Min = 65536;
 //   for (int coin : coins) {
@@ -94,13 +61,11 @@ int dp(vector<int> &coins, int amount) {
 
 int coinChange_with_vec(vector<int> &coins, int amount) {
   if (amount < 1) return 0;
-  count.resize(amount + 1);
+  count.assign(amount + 1, -2);
   return dp(coins, amount);
 }
 
 int main() {
-  // vector<int> coins = {186, 419, 83, 408};
-  // int ret = coinChange_with_vec(coins, 6249);
   vector<int> coins = {1, 2, 5};
   int ret = coinChange_with_vec(coins, 11);
   cout << "ret: " << ret << endl;
