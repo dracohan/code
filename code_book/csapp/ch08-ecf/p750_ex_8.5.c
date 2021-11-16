32cc5bf2cac2881352720fd9fb26661970e1f22e
dracohan <13240943@qq.com>
2021-11-16 09:19:52 +0800

    update@PT20211116 82.4kg

---
+++ code_book/csapp/ch08-ecf/p750_ex_8.5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+unsigned int snooze(unsigned int secs) {
+  unsigned int rc = sleep(secs);
+
+  printf("Slept for %d of %d secs\n", secs - rc, secs);
+  return rc;
+}
+
+int main() {
+  snooze(5);
+}
