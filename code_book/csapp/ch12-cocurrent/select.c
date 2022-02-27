e2ed392f808ddb63f06362e9c617ffb9cc138b01
dracohan <13240943@qq.com>
2022-02-27 20:08:38 +0800

    update@PT 20220227

--- code_book/csapp/ch12-cocurrent/select.c
+++ code_book/csapp/ch12-cocurrent/select.c
@@ -21,6 +21,11 @@ int main(int argc, char **argv)
     FD_SET(listenfd, &read_set);     /* Add listenfd to read set */ //line:conc:select:addlistenfd
 
     while (1) {
+        
+    FD_ZERO(&read_set);              /* Clear read set */ //line:conc:select:clearreadset
+    FD_SET(STDIN_FILENO, &read_set); /* Add stdin to read set */ //line:conc:select:addstdin
+    FD_SET(listenfd, &read_set);     /* Add listenfd to read set */ //line:conc:select:addlistenfd
+
 	ready_set = read_set;
 	Select(listenfd+1, &ready_set, NULL, NULL, NULL); //line:conc:select:select
 	if (FD_ISSET(STDIN_FILENO, &ready_set)) //line:conc:select:stdinready
