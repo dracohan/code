29d3ea41ba36fd15bd21d530364b10488d77d68a
Baowei Han <Baowei.Han@streamcomputing.com>
2023-08-28 17:07:53 +0800

    update@O

---
+++ code_book/labuladong/dp/45.跳跃游戏-ii.cpp
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode.cn id=45 lang=cpp
+ *
+ * [45] 跳跃游戏 II
+ *
+ * https://leetcode.cn/problems/jump-game-ii/description/
+ *
+ * algorithms
+ * Medium (45.10%)
+ * Likes:    2239
+ * Dislikes: 0
+ * Total Accepted:    535.6K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[2,3,1,1,4]'
+ *
+ * 给定一个长度为 n 的 0 索引整数数组 nums。初始位置为 nums[0]。
+ * 
+ * 每个元素 nums[i] 表示从索引 i 向前跳转的最大长度。换句话说，如果你在 nums[i] 处，你可以跳转到任意 nums[i + j]
+ * 处:
+ * 
+ * 
+ * 0 <= j <= nums[i] 
+ * i + j < n
+ * 
+ * 
+ * 返回到达 nums[n - 1] 的最小跳跃次数。生成的测试用例可以到达 nums[n - 1]。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: nums = [2,3,1,1,4]
+ * 输出: 2
+ * 解释: 跳到最后一个位置的最小跳跃数是 2。
+ * 从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: nums = [2,3,0,1,4]
+ * 输出: 2
+ * 
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 1 <= nums.length <= 10^4
+ * 0 <= nums[i] <= 1000
+ * 题目保证可以到达 nums[n-1]
+ * 
+ * 
+ */
+
+// @lc code=start
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <climits>
+#include <queue>
+#include <unordered_set>
+#include <unordered_map>
+
+#include "../utils/utils.h"
+#include "../utils/tree.h"
+
+using namespace std;
+
+
+class Solution {
+public:
+  bool jump(vector<int> &nums) {
+    int n = nums.size();
+    int end = 0, farthest = 0;
+    int jumps = 0;
+    for (int i = 0; i < n - 1; i++) {
+      farthest = max(nums[i] + i, farthest);
+      if (end == i) {
+        jumps++;
+        end = farthest;
+      }
+    }
+    return jumps;
+  }
+};
+
+int main() {
+  vector<int> coins = {2,3,1,5,1,4,1};
+  Solution s;
+  int ret = s.jump(coins);
+  std::cout << "ret:" << ret << std::endl;
+}
+// @lc code=end
+
--- code_book/labuladong/tree/102.二叉树的层序遍历.stack.cpp
+++ code_book/labuladong/tree/102.二叉树的层序遍历.stack.cpp
@@ -13,40 +13,41 @@
  * Total Submissions: 1.3M
  * Testcase Example:  '[3,9,20,null,null,15,7]'
  *
- * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
- * 
- * 
- * 
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。
+ * （即逐层地，从左到右访问所有节点）。
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：root = [3,9,20,null,null,15,7]
  * 输出：[[3],[9,20],[15,7]]
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：root = [1]
  * 输出：[[1]]
- * 
- * 
+ *
+ *
  * 示例 3：
- * 
- * 
+ *
+ *
  * 输入：root = []
  * 输出：[]
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 树中节点数目在范围 [0, 2000] 内
  * -1000 <= Node.val <= 1000
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -58,39 +59,48 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 #include <algorithm>
+#include <climits>
 #include <iostream>
 #include <map>
-#include <vector>
-#include <climits>
 #include <queue>
-#include <unordered_set>
 #include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
-#include "../utils/utils.h"
 #include "../utils/tree.h"
+#include "../utils/utils.h"
 
 using namespace std;
 
 class Solution {
-    vector<vector<int>> res;
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        
+  vector<int> levelOrder(TreeNode *root) {
+    queue<TreeNode *> q;
+    q.push(root);
+    vector<int> level;
+    while (!q.empty()) {
+      TreeNode *tmp = q.front();
+      q.pop();
+      level.push_back(tmp->val);
+      if (tmp->left != nullptr)
+        q.push(tmp->left);
+      if (tmp->right != nullptr)
+        q.push(tmp->right);
     }
+    return level;
+  }
 };
 
 int main() {
-  vector<int> vec = {3,9,20,8888,8888,15,7};
-  TreeNode* t = buildFrom(vec);
-  Solution s; 
+  vector<int> vec = {3, 9, 20, 8888, 8888, 15, 7};
+  TreeNode *t = buildFrom(vec);
+  Solution s;
   auto res = s.levelOrder(t);
-  for (auto r : res) {
-      printCollection(r);
-  }
+  printCollection(res);
 }
 // @lc code=end
-
